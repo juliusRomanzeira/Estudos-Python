5a40916d8735a3caabe7e3e4005b75a1306d872c
JuliusRomanzeira <julius.romanzeira@gmail.com>
2022-07-05 14:42:24 -0300

    Até o item 2 da Aula-1.py

---
+++ Aula-1.py
@@ -0,0 +1,11 @@
+# Crie um programa que leia o nome completo de uma pessoa e mostre:
+#
+# - O nome com todas as letras maiúsculas e minúsculas.
+#
+# - Quantas letras ao todo (sem considerar espaços)
+#
+# - Quantas letras tem o primeiro nome.
+#
+nome = input("\n\nDigite seu nome completo: ")
+print("\nNome em caixa alta --> {}\nNome em minúsculas --> {}".format(nome.upper,nome.lower))
+print("\nTamanho da string --> {}".format(nome.len()))
