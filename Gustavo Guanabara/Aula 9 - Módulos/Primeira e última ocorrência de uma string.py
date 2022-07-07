cd4fa2a15fdb9767b728e7de39bae38f037ecd3f
JuliusRomanzeira <julius.romanzeira@gmail.com>
2022-07-07 20:15:14 -0300

    Exercício 26 - Problema no último print

--- Gustavo Guanabara/Aula 9 - Módulos/Primeira e última ocorrência de uma string.py
+++ Gustavo Guanabara/Aula 9 - Módulos/Primeira e última ocorrência de uma string.py
@@ -1,8 +1,12 @@
-frase, aux, pos, space = input("Digite uma frase: ").lower(), 0, [], 0
+frase, aux, esp, var = input("Frase: ").lower(), 0, 0, []
+while frase[esp] == ' ':
+    esp=esp+1
 for i in range(len(frase)):
     if frase[i] == 'a':
-        pos.insert(aux, i)
+        var.insert(aux, i)
         aux = aux+1
-print("A letra A aparece {} vezes na frase.\nA primeira letra A apareceu na posição {}".format(aux, pos[0]))
-# print("A última letra A apareceu na posição {}".format((pos[len(pos)-1])))
-# print(space)
+print("A letra A aparece {} vezes na frase".format(aux))
+print("A primeira letra A apareceu na posição {}".format((var[0]+1)-esp))
+# print(len(frase))
+# print(esp)
+# print("A última letra A apareceu na posição {}".format(var[aux]-esp))
