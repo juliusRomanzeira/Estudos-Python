04939e8f3b161cbb9f458edb4b39c419b3b95e4c
JuliusRomanzeira <julius.romanzeira@gmail.com>
2022-07-07 16:55:03 -0300

    Exercício 22 - Concluído (Comentado)

--- Gustavo Guanabara/Aula 9 - Módulos/Manipulando Textos.py
+++ Gustavo Guanabara/Aula 9 - Módulos/Manipulando Textos.py
@@ -3,4 +3,6 @@ print("Analizando seu nome...")
 print("Seu nome em maiúsculas é {}".format(nome.upper()))
 print("Seu nome em minúsculas é {}".format(nome.lower()))
 print("Seu nome tem ao todo {} letras".format(1+(len(nome)-len(nome.split()))))
+# Uma outra resposta para a linha 5 é:
+# print("Seu nome tem ao todo {} letras".format(len(nome) - nome.count(' ')))
 print("Seu primeiro nome é {} e tem {} letras".format(nome.split()[0], len(nome.split()[0])))
