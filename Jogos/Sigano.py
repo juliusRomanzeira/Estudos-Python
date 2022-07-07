6c95e437eecb755e114413b3fd350321bd244b08
JuliusRomanzeira <julius.romanzeira@gmail.com>
2022-07-06 21:27:26 -0300

    Perdi nada - Bora ver que digita mais !

--- Jogos/Sigano.py
+++ Jogos/Sigano.py
@@ -2,6 +2,16 @@ import random
 adv = float(input("Você está preso em um Genjutsu,\nPara sair você terá que acertar qual número estou pensando\nDigite um número: "))
 if adv>5 or adv<0:
     print("Digito fora do range!!")
-print("SE FODEU OTÁRIO, ROBEI TEU REPOSITORIO")
-
-# HAHAHAHAHAHAHAHHAHAAHAHHA
+else:
+    cpu = float(random.choice(range(0,6)))
+    while True:
+        if cpu == adv:
+            print("KRLLL !! Tu é bom mesmo !!")
+            break
+        print("\n\nERRRROOOOOOUUU!")
+        cpu = float(random.choice(range(0,6)))
+        print("\nPensei no número {}\n\nTente novamente, loser !!".format(int(cpu)))
+        adv = float(input("Você está preso em um Genjutsu,\nPara sair você terá que acertar qual número estou pensando\nDigite um número: "))
+        if adv>5 or adv<0:
+            print("Digito fora do range!!")
+            break
