97aa69f3d86576598d5a4447d8511d33a0a83e15
JuliusRomanzeira <julius.romanzeira@gmail.com>
2022-07-09 17:24:13 -0300

    Exercício 26 - Concluído (Comentado)

--- Gustavo Guanabara/Aula 9 - Módulos/Primeira e última ocorrência de uma string.py
+++ Gustavo Guanabara/Aula 9 - Módulos/Primeira e última ocorrência de uma string.py
@@ -1,12 +1,16 @@
-frase, aux, esp, var = input("Frase: ").lower(), 0, 0, []
-while frase[esp] == ' ':
-    esp=esp+1
-for i in range(len(frase)):
-    if frase[i] == 'a':
-        var.insert(aux, i)
-        aux = aux+1
-print("A letra A aparece {} vezes na frase".format(aux))
-print("A primeira letra A apareceu na posição {}".format((var[0]+1)-esp))
-# print(len(frase))
-# print(esp)
-# print("A última letra A apareceu na posição {}".format(var[aux]-esp))
+frase, aux, i, pos = input("Digite uma frase: ").lower(), [], 0, 0
+print("A letra A aparece {} na frase.".format(frase.count('a')))
+while frase[i] == ' ':
+    i=i+1
+for j in range(len(frase)):
+    if frase[j]=='a':
+        aux.insert(pos, j+1)
+        pos = pos+1
+print("A primeira letra A apareceu na posição {}".format(aux[0]-i))
+print("A última letra A apareceu na posição {}".format(aux[pos-1]-i))
+
+# Uma outra forma:
+# frase = input("Digite uma frase: ").lower()
+# print("A letra A aparece {} vezes na frase".forma(frase.count('a')))
+# print("A primeira letra A apareceu na posição {}".format(frase.find('a')+1))
+# print("A última letra A apareceu na posição {}".format(frase.rfind('a')+1))
