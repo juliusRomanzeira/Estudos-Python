c3d3558a04f72cff7c0c847b24d70088b1d55280
JuliusRomanzeira <julius.romanzeira@gmail.com>
2022-07-10 11:57:42 -0300

    Aula 10 - Exemplo 1

---
+++ Gustavo Guanabara/Aula 10 - Condições/Aula Teórica.py
@@ -0,0 +1,6 @@
+nome = input("Qual seu nome ? ")
+nomelow = nome.lower()
+if nomelow == 'julius':
+    print("Que nome lindo, meu chará !")
+else:
+    print("Prazer em te conhecer {}".format(nome))
