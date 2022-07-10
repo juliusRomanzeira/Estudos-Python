fa4254b3d35979fde330df7e506d361fa865662e
JuliusRomanzeira <julius.romanzeira@gmail.com>
2022-07-10 12:07:22 -0300

    Aula 10 - Exemplo 2

--- Gustavo Guanabara/Aula 10 - Condições/Aula Teórica.py
+++ Gustavo Guanabara/Aula 10 - Condições/Aula Teórica.py
@@ -1,6 +1,11 @@
-nome = input("Qual seu nome ? ")
-nomelow = nome.lower()
-if nomelow == 'julius':
-    print("Que nome lindo, meu chará !")
+n1 = float(input("Digite a primeira nota: "))
+n2 = float(input("Digite sua segunda nota: "))
+m = (n1+n2)/2
+if m<7:
+    print("Xiii, bora pra final!")
+elif m ==7:
+    print("Passou raspando, hein?")
+elif m>7:
+    print("Aí sim boy! Passou acima da média!")
 else:
-    print("Prazer em te conhecer {}".format(nome))
+    print("Isso aí tá fora do range, Manny meu chapa!")
