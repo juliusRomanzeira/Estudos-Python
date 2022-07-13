b5505d488cb9331a7de3318b806013f09a2e535e
JuliusRomanzeira <julius.romanzeira@gmail.com>
2022-07-13 14:55:19 -0300

    Aula 12 - Mundo 2 - Python

---
+++ Gustavo Guanabara/Aula 12 - Condições Aninhadas/Aula 12 - Exemplo 01.py
@@ -0,0 +1,8 @@
+nome = input("Digite seu nome: ")
+if nome.lower() == 'julius':
+    print("Que nome Lindo !")
+elif nome.lower() == 'João' or nome.lower() == 'Maria' or nome.lower() == 'Paulo':
+    print("Seu nome é muito comum no Brasil !")
+else:
+    print("Que nome feio")
+print("Tenha um bom dia!!")
